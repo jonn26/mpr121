851816a0e3beab4d518d758d1081373fe54e9a47
jonn26 <jonn26@gmail.com>
2017-11-15 22:34:43 -0700

    change dir structure and rem wire begin

--- src/MPR121.cpp
+++ src/MPR121.cpp
@@ -49,8 +49,7 @@ extern "C" {
 #define OVERCURRENT_FLAG_BIT 3
 #define OUT_OF_RANGE_BIT 4
 
-MPR121_t::MPR121_t(){
-	Wire.begin();	
+MPR121_t::MPR121_t(){	
 	address = 0x5C;    // default address is 0x5C, for use with Bare Touch Board
 	ECR_backup = 0x00;
 	running = false;
